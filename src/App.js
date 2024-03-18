bda3d157cc26fa59dc8a308da159b1a45528bad6
mehdi hou <mehdi5juillet@gmail.com>
2024-03-19 00:54:09 +0100

    mehdi client setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Aissah
+         MehdiHou
         </p>
         <a
           className="App-link"
