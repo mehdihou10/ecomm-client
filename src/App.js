ab34f061f0dc02a327ca83c8160f73af63ec7a39
Aissahouar <aissahouar4@gmail.com>
2024-03-19 00:52:16 +0100

    aissa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Aissa
+         Aissah
         </p>
         <a
           className="App-link"
