67564280a8d0653f3113eb535c5726c829588ef5
Aissahouar <aissahouar4@gmail.com>
2024-03-21 01:55:28 +0100

    i added twilind componnets

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import {Home,Products,ProductDetails,
-        Shop,Checkout,WishList,
-        UserDashboard,VendorDashboard,AdminDashboard,Contact,
-        Signup,Login,CheckboxType} from './pages';
+import {
+  Home,
+  Products,
+  ProductDetails,
+  Shop,
+  Checkout,
+  WishList,
+  UserDashboard,
+  VendorDashboard,
+  AdminDashboard,
+  Contact,
+  Signup,
+  Login,
+  CheckboxType,
+} from "./pages";
 
-
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="auth/login" element={<Login />} />
+        <Route path="auth/type" element={<CheckboxType />} />
+        <Route path="auth/signup" element={<Signup />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:productId" element={<ProductDetails />} />
+        <Route path="cart" element={<Shop />} />
+        <Route path="wishlist" element={<WishList />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="user_dashboard/:username" element={<UserDashboard />} />
+        <Route
+          path="vendor_dashboard/:username"
+          element={<VendorDashboard />}
+        />
+        <Route path="admin_dashboard/:username" element={<AdminDashboard />} />
 
-    <Routes>
-
-      <Route path='/' element={<Home />} />
-      <Route path='auth/login' element={<Login />} />
-      <Route path='auth/type' element={<CheckboxType />} />
-      <Route path='auth/signup' element={<Signup />} />
-      <Route path='products' element={<Products />} />
-      <Route path='products/:productId' element={<ProductDetails />} />
-      <Route path='cart' element={<Shop />} />
-      <Route path='wishlist' element={<WishList />} />
-      <Route path='checkout' element={<Checkout />} />
-      <Route path='user_dashboard/:username' element={<UserDashboard />} />
-      <Route path='vendor_dashboard/:username' element={<VendorDashboard />} />
-      <Route path='admin_dashboard/:username' element={<AdminDashboard />} />
-      
-      <Route path='Contact' element={<Contact />} />
-
-
-    </Routes>
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboard from '../components/Dasboard'
 
 const AdminDashboard = () => {
   return (
     <div>
-      admin dashboard
+      <Dashboard></Dashboard>
     </div>
   )
 }
