f8e761609359e204f837b918854ef0aaff2d38cd
Aissahouar <aissahouar4@gmail.com>
2024-03-23 02:36:11 +0100

    checkbox page beta version

--- src/pages/CheckboxType.jsx
+++ src/pages/CheckboxType.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaStore } from "react-icons/fa";
+import { FaPeopleCarry } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa6";
+import { LuHeading1 } from "react-icons/lu";
 
 const CheckboxType = () => {
-  return (
-    <div>
-      CheckboxType
-    </div>
-  )
-}
+  const [type, setType] = useState("");
 
-export default CheckboxType
+  const changeType = (type) => {
+    setType(type);
+    console.log(type);
+  };
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-24 lg:px-8 relative">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Choose what you want to be
+          </h2>
+        </div>
+        <div className="mt-10 flex justify-center  flex-wrap gap-10">
+          {type === "client" ? (
+            <div
+              onClick={() => changeType("client")}
+              className="container w-64 h-64 flex flex-col flex-grow items-center  py-2 rounded-xl hover:shadow-lg bg-main"
+            >
+              <p className="font-semibold text-white text-xl ">Client</p>
+              <FaCartPlus className="text-9xl text-white mt-8"></FaCartPlus>
+            </div>
+          ) : (
+            <div
+              onClick={() => changeType("client")}
+              className="container w-64 h-64 flex flex-col flex-grow items-center  py-2 rounded-xl hover:shadow-lg bg-white"
+            >
+              <p className="font-semibold text-xl">Client</p>
+              <FaCartPlus className="text-9xl text-main mt-8"></FaCartPlus>
+            </div>
+          )}
+
+          {type === "vendor" ? (
+            <div
+              onClick={() => changeType("vendor")}
+              className="container w-64 h-64 rounded-xl flex flex-grow items-center flex-col py-2  hover:shadow-lg bg-main"
+            >
+              <p className="font-semibold text-xl text-white">Vendor</p>
+              <Link to="/auth/signup">
+                <FaStore className="text-9xl text-white mt-8"></FaStore>
+              </Link>
+            </div>
+          ) : (
+            <div
+              onClick={() => changeType("vendor")}
+              className="container w-64 h-64 rounded-xl flex flex-grow items-center flex-col py-2  hover:shadow-lg bg-white"
+            >
+              <p className="font-semibold text-lg">Vendor</p>
+              <FaStore className="text-9xl text-main mt-8"></FaStore>
+            </div>
+          )}
+        </div>
+
+        <div className="mt-4 absolute bottom-2 right-10 ">
+          {type === "client" || type === "vendor" ? (
+            <Link to="/auth/signup">
+              <button className="bg-main py-2 px-6 font-semibold text-white">
+                Next
+              </button>
+            </Link>
+          ) : (
+            <button className="bg-indigo-400 py-2 px-6 cursor-default font-semibold text-white">
+              Next
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CheckboxType;
