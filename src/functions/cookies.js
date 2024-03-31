1347072f820b258095c127447ff4e46ee5a835a8
Aissahouar <aissahouar6@gmail.com>
2024-03-31 01:10:58 +0100

    added

--- src/functions/cookies.js
+++ src/functions/cookies.js
@@ -1,13 +1,19 @@
-export const isCookieExists = ()=>{
+export const isCookieExists = () => {
+  let findCookie = document.cookie
+    .split(";")
+    .filter((ck) => ck.includes("user"));
 
-    let findCookie = document.cookie.split(';').filter(ck=>ck.includes('user'));
-    
-    return findCookie.length === 0 ? false : findCookie[0].split('=')[1] === null ? false : true;
-    
-}
+  return findCookie.length === 0
+    ? false
+    : findCookie[0].split("=")[1] === null
+    ? false
+    : true;
+};
 
-export const getCookie = ()=>{
-
-    const mainCookie = document.cookie.split(';').filter(ck=>ck.includes('user'))[0].split('=')[1];
-    return mainCookie;
-}
+export const getCookie = () => {
+  const mainCookie = document.cookie
+    .split(";")
+    .filter((ck) => ck.includes("user"))[0]
+    .split("=")[1];
+  return mainCookie;
+};
