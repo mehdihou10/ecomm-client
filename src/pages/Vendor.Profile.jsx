f9b6e28ca184eef2a7b3ce246c5325eba29bf4c5
mehdi hou <mehdi5juillet@gmail.com>
2024-04-02 02:10:39 +0200

    add profile

---
+++ src/pages/Vendor.Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DashboardSidebar from '../components/Dashboard.Sidebar'
+
+const VendorProfile = () => {
+  return (
+    <div className='flex gap-[20px]'>
+
+        <DashboardSidebar />
+
+        profile
+      
+    </div>
+  )
+}
+
+export default VendorProfile
--- src/pages/VendorDashboard.jsx
+++ src/pages/VendorDashboard.jsx
@@ -1,20 +1,15 @@
 import SideBar from "../components/vendor.sidebar";
 import DashboardSidebar from "../components/Dashboard.Sidebar";
-import {Routes,Route} from 'react-router-dom';
-import {Home,Contact} from '../pages';
+
 
 const VendorDashboard = () => {
 
   return (
-    // <SideBar></SideBar>
-    <div className="flex">
+
+<div className="flex">
       <DashboardSidebar />
 
-      {/* <Routes>
-        <Route path="/vendor_dashboard/:username/home" element={<Contact />} />
-        <Route path="/vendor_dashboard/:username/contact" element={<Home />} />
-
-      </Routes> */}
+      
     </div>
   );
 };
