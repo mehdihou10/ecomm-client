fa9b42d64d10ff08a93b35be8a219ffd15490d5b
mehdi hou <mehdi5juillet@gmail.com>
2024-04-04 01:49:14 +0200

    refactor size

--- src/pages/vendor.update.jsx
+++ src/pages/vendor.update.jsx
@@ -87,7 +87,7 @@ const VendorUpdate = () => {
   return (
     <div className="flex gap-[20px]">
       <DashboardSidebar />
-      <div className="py-20 px-[20px] bg-white">
+      <div className="py-20 px-[20px] bg-white flex-1">
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
